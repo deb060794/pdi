ade332d268775c9731f77596a8830db497b07e03
tinsia Debrah <debrah.tinsia@infrabel.be>
2023-03-28 16:41:50 +0200

    Modèle Artist + migration + données initiales

---
+++ src/main/java/be/iccbxl/pid/model/ArtistRepository.java
@@ -0,0 +1,10 @@
+package be.iccbxl.pid.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ArtistRepository extends CrudRepository<Artist, Long> {
+    List<Artist> findByLastname(String lastname);
+    Artist findById(long id);
+}
