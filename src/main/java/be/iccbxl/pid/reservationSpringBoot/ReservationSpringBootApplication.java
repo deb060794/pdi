dcd67807abb5d9af6c78c2534298f573503edef3
tinsia Debrah <debrah.tinsia@infrabel.be>
2023-03-28 16:05:47 +0200

    After framework installation

---
+++ src/main/java/be/iccbxl/pid/reservationSpringBoot/ReservationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.reservationSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/iccbxl/pid/reservationSpringBoot/ReservationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.reservationSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
