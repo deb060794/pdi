9fbc7398323a8415007af67c8851aa8108869810
tinsia Debrah <debrah.tinsia@infrabel.be>
2023-03-30 18:10:05 +0200

    Relation ManyToMany between Artist & Type

--- src/main/java/be/iccbxl/pid/reservationSpringBoot/model/Type.java
+++ src/main/java/be/iccbxl/pid/reservationSpringBoot/model/Type.java
@@ -2,6 +2,9 @@ package be.iccbxl.pid.reservationSpringBoot.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "types")
 public class Type {
@@ -9,6 +12,15 @@ public class Type {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String type;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artist_type",
+            joinColumns = @JoinColumn(name = "type_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
+
+
 
     protected Type() { }
 
@@ -27,6 +39,29 @@ public class Type {
     public void setType(String type) {
         this.type = type;
     }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public Type addArtist(Artist artist) {
+        if(!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this);
+        }
+
+        return this;
+    }
+
+    public Type removeType(Artist artist) {
+        if(this.artists.contains(artist)) {
+            this.artists.remove(artist);
+            artist.getTypes().remove(this);
+        }
+
+        return this;
+    }
+
 
     @Override
     public String toString() {
